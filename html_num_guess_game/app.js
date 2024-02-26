0509d3ce895639fde1fa314f4771b98324fbe146
LouisMartin07 <louis.martin11730@gmail.com>
2024-02-26 11:59:32 -0600

    adad

---
+++ html_num_guess_game/app.js
@@ -0,0 +1,27 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+let guessedNumbers = [];
+
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById('userGuess').value);
+
+    if (userGuess === randomNumber) {
+        showMessage('Congratulations! You guessed the correct number.');
+    } else {
+        guessedNumbers.push(userGuess);
+
+        if (userGuess < randomNumber) {
+            showMessage('Try guessing a higher number.');
+        } else {
+            showMessage('Try guessing a lower number.');
+        }
+    }
+    updateGuessList();
+}
+function showMessage(message) {
+    document.getElementById('message').innerText = message;
+}
+function updateGuessList() {
+    const guessList = document.getElementById('guessList');
+    guessList.innerHTML = '<strong>Your Guesses:</strong><br>' + guessedNumbers.join(', ');
+}
