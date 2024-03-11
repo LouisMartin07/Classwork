53d4989113fc5706b8673529a0e1c2cc7a37f581
LouisMartin07 <louis.martin11730@gmail.com>
2024-03-11 17:20:36 -0500

    integrating schema and csv data manipulation

---
+++ gameStoreSQL/SQLfiles/gamestore.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS game;
+
+CREATE TABLE game (
+    game_id SERIAL PRIMARY KEY,
+    game_title VARCHAR(255),
+    quantity INT,
+    price DECIMAL(5,2)
+);
+
+\COPY game FROM './data/game.csv' WITH CSV HEADER;
+
+DROP TABLE IF EXISTS action_figure;
+
+CREATE TABLE action_figure (
+    id SERIAL PRIMARY KEY,
+    action_figure_title VARCHAR,
+    quantity INT,
+    price DECIMAL(5,2)
+);
+
+\COPY action_figure FROM './data/action_figure.csv' WITH CSV HEADER;
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    employee_name VARCHAR,
+    position VARCHAR,
+    salary DECIMAL(7,2)
+);
+
+\COPY employee FROM './data/employee.csv' WITH CSV HEADER;
+
+DROP TABLE IF EXISTS poster;
+
+CREATE TABLE poster (
+    id SERIAL PRIMARY KEY,
+    poster_title VARCHAR,
+    quantity INT,
+    price DECIMAL(4,2)
+);
+
+\COPY poster FROM './data/poster.csv' WITH CSV HEADER;
+
