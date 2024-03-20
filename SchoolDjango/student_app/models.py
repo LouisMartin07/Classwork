23068f584f5101aabdc609cdce5e7df68b8edd69
LouisMartin07 <louis.martin11730@gmail.com>
2024-03-20 14:19:55 -0500

    changed tests and added validators

--- SchoolDjango/student_app/models.py
+++ SchoolDjango/student_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.core import validators as v
+from .validators import validate_name_format, validate_school_email,validate_combination_format
 
 class Student(models.Model):
-    name = models.CharField(max_length=255, blank=False, unique=False)
-    student_email = models.EmailField(unique=True, blank=False)
+    name = models.CharField(max_length=255, blank=False, unique=False, validators=[validate_name_format])
+    student_email = models.EmailField(unique=True, blank=False, validators = [validate_school_email])
     personal_email = models.EmailField(unique=True, blank=True, null=True)
-    locker_number = models.IntegerField(blank=False, unique=True, default=110)
-    locker_combination = models.CharField(max_length=10, blank=False, unique=False, default="12-12-12")
+    locker_number = models.IntegerField(blank=False, unique=True, default=110, validators =[v.MinValueValidator(1), v.MaxValueValidator(2)])
+    locker_combination = models.CharField(max_length=10, blank=False, unique=False, default="12-12-12", validators = [validate_combination_format])
     good_student = models.BooleanField(default=True)
 
 def __str__(self):
---
+++ SchoolDjango/student_app/validators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+import re
+
+def validate_name_format(value):
+    if not re.match(r'^[A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+$', value):
+        raise ValidationError('Name must be in the format "First M. Last"')
+
+def validate_school_email(value):
+    if not value.endswith('@school.com'):
+        raise ValidationError('Invalid school email format. Please use an email ending with "@school.com".')
+
+def validate_combination_format(value):
+    if not re.match(r'^\d{2}-\d{2}-\d{2}$', value):
+        raise ValidationError('Combination must be in the format "12-12-12"')
